2d1aac7be162edc32d876a4d36e8af039118f23e
Jung Jein <o0o1910419@sookmyung.ac.kr>
2021-11-10 01:18:31 +0900

    section5:  redux middleware(3)

--- ReduxSimpleWeather/src/actions/index.js
+++ ReduxSimpleWeather/src/actions/index.js
@@ -10,8 +10,9 @@ export function fetchWeather(city) {
     const url = `${ROOT_URL}&q=${city},us`;
     const request = axios.get(url);
 
+
     return {
-        type: 'FETCH_WEATHER',
+        type: FETCH_WEATHER,
         payload: request
     };
- }
+}
--- ReduxSimpleWeather/src/reducers/index.js
+++ ReduxSimpleWeather/src/reducers/index.js
@@ -1,8 +1,9 @@
 import { combineReducers } from 'redux';
-import BooksReducer from './reducer_book';
+import WeatherReducer from './reducer_weather';
+
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  weather: WeatherReducer
 });
 
 export default rootReducer;
